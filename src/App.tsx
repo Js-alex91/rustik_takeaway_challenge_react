23e22cb54c7dbb7011c6df3e869d864a1b8e4f34
Jonatan Alexis Severo <alexisssevero@gmail.com>
2022-08-30 19:40:16 -0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,17 @@
 import React from 'react'
 import { Provider } from 'react-redux'
-
 import { MovieCard } from './components/card/MovieCard'
 import { Header } from './components/header/Header';
 import { store } from './redux/store/store'
 import { Container } from './views/Container'
 import { ContainerList } from './views/ContainerList';
 
-
 export const App = () => {
-
 
   return (
     <Provider store={store}>
       <Container>
-
-
         <Header />
-
         <ContainerList />
       </Container>
     </Provider>
